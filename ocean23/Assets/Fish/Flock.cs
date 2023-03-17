bbdb768d21b59671c496dcc6d56c0965e82b1114
Chenrui ZHU <chenrui.zhu@etu.univ-nantes.fr>
2023-03-17 22:05:38 +0100

    Update fish scene

--- ocean23/Assets/Fish/Flock.cs
+++ ocean23/Assets/Fish/Flock.cs
@@ -1,126 +1,150 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Flock : MonoBehaviour
 {
+    public TextMeshProUGUI Text;
+    
+    // score
+    public static int score;
+    
     public float speed = 1.0f;
     public float rotationSpeed = 10.0f;
     public float followSpeed = 0.1f;
     Vector3 averageHeading;
     Vector3 averagePosition;
-    float neighbourDistance = 20.0f;
+    // float neighbourDistance = 20.0f;
     Vector3 goalPos = globalFlock.goalPos;
     public float angleSpeed = 0.01f;
     public bool isRotate = true;
 
-    // Transform parentTransform;
-    // Transform childTransform;
-
+    public static float w = 0.3f;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = Random.Range(20.0f,50);
-        // parentTransform = GameObject.Find("Parent").GetComponent<Transform>();
-        // childTransform = GameObject.Find("Child").GetComponent<Transform>();
-        // childTransform.transform.parent = parentTransform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, new Vector3(0,0, transform.position.z))>200){
-            Vector3 direction = Vector3.zero - new Vector3(transform.position.x,0,transform.position.z);
+        if(transform.position.x>200 || transform.position.x<-200){
+            // Vector3 direction = Vector3.zero - new Vector3(transform.position.x,0,transform.position.z);
             transform.Rotate(0.0f, 0.0f, rotationSpeed * 180.0f);
-            transform.Translate(speed * Time.deltaTime,0,0);
+        }
+        if(Vector3.Distance(transform.position,new Vector3(0,20,transform.position.z))<=30){
+            if(Random.Range(0,3)<1){
+                transform.RotateAround(new Vector3(0,10,transform.position.z),Vector3.forward,100.0f*Time.deltaTime);
+            }
+            else{
+                transform.RotateAround(new Vector3(0,2,transform.position.z),Vector3.forward,100.0f*Time.deltaTime);
+            }
         }
         else{
-            // rotation angle
-            if(Random.Range(0,20000)<20){
-                transform.Rotate(0.0f, 1.0f, 0.0f);
-            }
-            if(Random.Range(0,20000)<20){
-                transform.Rotate(0.0f, -1.0f, 0.0f);
-            }
-            // turn back
-            if(Random.Range(0,20000)<20){
-                transform.Rotate(0.0f, 0.0f, rotationSpeed * 180.0f);
-            }
-
-            // avoid
-            // if(this.transform.position.z == 30){
-            //     transform.Rotate(0.0f, 0.0f, rotationSpeed * 180.0f);
-            //     transform.Translate(speed * Time.deltaTime,0,0);
-            // }
-
-            // follow
-            // ApplyFollow(goalPos);
-        
             transform.Translate(speed * Time.deltaTime,0,0);
         }
+        
+        // else{
+        //     // rotation angle
+        //     if(Random.Range(0,20000)<20){
+        //         transform.Rotate(0.0f, 1.0f, 0.0f);
+        //     }
+        //     if(Random.Range(0,20000)<20){
+        //         transform.Rotate(0.0f, -1.0f, 0.0f);
+        //     }
     }
 
-    void ApplyFollow(Vector3 goalPos){
-        bool isRotate = true;
-        if (isRotate)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(goalPos-transform.position),rotationSpeed*Time.deltaTime);
-            transform.position = Vector3.MoveTowards(transform.position, goalPos, followSpeed * Time.deltaTime);    
-        }
-        if (Vector3.Angle(goalPos, transform.position) < 0.1f)
-        {
-            isRotate = false;
-        }
+    // counter
+    void OnTriggerEnter(Collider collider) {
+        Vector3 pos = this.transform.position;
+        pos.x = -100;
+        this.transform.position = pos;
+        this.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f,1);
+        score += 1;
+
+        Text.text = score.ToString() + " POINTS";
+
     }
 
-    void ApplyRule(){
-        GameObject[] gos;
-        gos = globalFlock.allfish;
+    // escape
+    // void FlockEscape(){
+    //     this.transform.Rotate(0.0f, 0.0f, rotationSpeed * 180.0f);
+    //     // Vector3 pos = go.transform.position;
+    //     // pos.x += -1 * 30 * Time.deltaTime;
+    //     // go.transform.position = pos;
+    // }
 
-        Vector3 vcentre = new Vector3(10,10,10);
-        Vector3 vavoid = Vector3.zero;
-        float gSpeed = 0.1f;
+    // void OnTriggerStay(Collider collider) {
+    //     transform.RotateAround(new Vector3(0,Vector3.Distance(transform.position,new Vector3(0,10,0)),transform.position.z),Vector3.forward,200.0f*Time.deltaTime);
+    // }
 
-        Vector3 goalPos = globalFlock.goalPos;
-        int tankSize = globalFlock.tankSize;
+    // void ApplyRule(){
+    //     GameObject[] gos;
+    //     gos = globalFlock.allfish;
 
-        float dist;
-        int groupSize = 0;
+    //     Vector3 vcentre = new Vector3(10,10,10);
+    //     Vector3 vavoid = Vector3.zero;
+    //     float gSpeed = 0.1f;
 
-        foreach (GameObject go in gos)
-        {
-            if(go != this.gameObject){
-                dist = Vector3.Distance(go.transform.position,this.transform.position);
-                if(dist<=neighbourDistance){
-                    vcentre += go.transform.position;
-                    groupSize++;
+    //     Vector3 goalPos = globalFlock.goalPos;
+    //     int tankSize = globalFlock.tankSize;
 
-                    if(dist<1.0f){
-                        vavoid = vavoid + (this.transform.position - go.transform.position);
-                    }
+    //     float dist;
+    //     int groupSize = 0;
 
-                    Flock anotherFlock = go.GetComponent<Flock>();
-                    gSpeed = gSpeed + anotherFlock.speed;
-                }
-            }
-        }
+    //     foreach (GameObject go in gos)
+    //     {
+    //         if(go != this.gameObject){
+    //             dist = Vector3.Distance(go.transform.position,this.transform.position);
+    //             if(dist<=neighbourDistance){
+    //                 vcentre += go.transform.position;
+    //                 groupSize++;
 
-        if (groupSize>0){
-            vcentre = vcentre/groupSize + (goalPos - this.transform.position);
-            speed = gSpeed/groupSize;
+    //                 if(dist<1.0f){
+    //                     vavoid = vavoid + (this.transform.position - go.transform.position);
+    //                 }
 
-            // Vector3 direction = goalPos - this.transform.position;
-            // Vector3 direction = vcentre + vavoid;
+    //                 Flock anotherFlock = go.GetComponent<Flock>();
+    //                 gSpeed = gSpeed + anotherFlock.speed;
+    //             }
+    //         }
+    //     }
+
+    //     if (groupSize>0){
+    //         vcentre = vcentre/groupSize + (goalPos - this.transform.position);
+    //         speed = gSpeed/groupSize;
+
+    //         // Vector3 direction = goalPos - this.transform.position;
+    //         Vector3 direction = vcentre + vavoid;
             
-            Vector3 direction = new Vector3(Random.Range(-tankSize,tankSize),0,Random.Range(-tankSize,tankSize));
-            direction += vcentre + vavoid;
-            if(direction != Vector3.zero){
-                transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(goalPos-transform.position),rotationSpeed*Time.deltaTime);
-            }
-        }
-    }
+    //         // Vector3 direction = new Vector3(Random.Range(-tankSize,tankSize),0,Random.Range(-tankSize,tankSize));
+    //         direction += vcentre + vavoid;
+    //         if(direction != Vector3.zero){
+    //             transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(),rotationSpeed*Time.deltaTime);
+    //         }
+    //     }
+    // }
 }
+
+
+
+    // void ApplyFollow(Vector3 goalPos){
+    //     bool isRotate = true;
+    //     if (isRotate)
+    //     {
+    //         transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(goalPos-transform.position),rotationSpeed*Time.deltaTime);
+    //         transform.position = Vector3.MoveTowards(transform.position, goalPos, followSpeed * Time.deltaTime);    
+    //     }
+    //     if (Vector3.Angle(goalPos, transform.position) < 0.1f)
+    //     {
+    //         isRotate = false;
+    //     }
+    // }
+
+    
 
 
         // Vector3 vec = -goalPos;
--- ocean23/Assets/Fish/globalFlock.cs
+++ ocean23/Assets/Fish/globalFlock.cs
@@ -6,9 +6,9 @@ public class globalFlock : MonoBehaviour
 {
     public GameObject fishPrefab;
     public GameObject goalPrefab;
-    public static int tankSize = 50;
+    public static int tankSize = 100;
 
-    static int numFish = 1;
+    static int numFish = 50;
     public static GameObject[] allfish = new GameObject[numFish];
 
     public static Vector3 goalPos;
@@ -17,24 +17,14 @@ public class globalFlock : MonoBehaviour
     void Start()
     {
         for(int i = 0; i<numFish; i++){
-            Vector3 pos = new Vector3(-10,0,Random.Range(tankSize/2,tankSize/2+5));
+            Vector3 pos = new Vector3(Random.Range(-200,-100),0,Random.Range(tankSize/2-15,tankSize/2-5));
             allfish[i] = (GameObject) Instantiate(fishPrefab, pos, Quaternion.identity);
         }
-
-        goalPrefab.transform.position = new Vector3(-100,0,10);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Random.Range(1,20000)<20){
-            goalPrefab.transform.Rotate(0.0f,1.0f,0.0f);
-        }
-        if(Random.Range(1,20000)<20){
-            goalPrefab.transform.Rotate(0.0f,1.0f,0.0f);
-        }
-        goalPrefab.transform.Translate(10.0f * Time.deltaTime,0,0);
-        goalPos = goalPrefab.transform.position;
     }
 }
